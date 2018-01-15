950070f2ef2e0db731e94f07eaf86a661de3d86a
Sean Blundell <seanblundell@gmail.com>
2018-01-15 16:58:09 +0000

    Write tests for isSpare function

---
+++ src/utilities/isSpare.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import isSpare from './isSpare'
+import {shallow} from 'enzyme'
+
+describe('isSpare function', () => {
+  it('should return true if spare', () => {
+    for (var i = 0; i < 11; i++) {
+      expect(isSpare(i, 10 - i)).toEqual(true)
+    }
+  })
+
+  it('should return false if not spare', () => {
+    for(var i = 0; i < 9; i=i+2) {
+      expect(isSpare(i, 1)).toEqual(false)
+    }
+  })
+})
