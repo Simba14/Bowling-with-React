417e496366503d7cf7c7ce4dc0e53e6f2b19f28b
Sean Blundell <seanblundell@gmail.com>
2018-01-15 16:48:39 +0000

    Write tests fo isEven function

--- src/utilities/isEven.test.js
+++ src/utilities/isEven.test.js
@@ -4,6 +4,14 @@ import {shallow} from 'enzyme'
 
 describe('isEven function', () => {
   it('should return true if even number', () => {
-    
+    for(var i = 0; i < 100; i=i+2) {
+      expect(isEven(i)).toEqual(true)
+    }
+  })
+
+  it('should return false if not bonus roll', () => {
+    for(var i = 1; i < 100; i=i+2) {
+      expect(isEven(i)).toEqual(false)
+    }
   })
 })
